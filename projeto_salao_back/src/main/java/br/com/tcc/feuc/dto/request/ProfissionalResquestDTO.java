71be6238794ac15851c1ed66b5c57f0d408291e7
Diego Bizerra da Silva <diegorj93@gmail.com>
2024-02-09 16:45:33 -0300

    Melhorias na classe profissional

--- projeto_salao_back/src/main/java/br/com/tcc/feuc/dto/request/ProfissionalResquestDTO.java
+++ projeto_salao_back/src/main/java/br/com/tcc/feuc/dto/request/ProfissionalResquestDTO.java
@@ -1,18 +1,35 @@
 package br.com.tcc.feuc.dto.request;
 
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import lombok.Data;
 
 @Data
 public class ProfissionalResquestDTO {
 
+	@Size(min = 6, max = 100, message = "Nome deve ter de 6 a 100 caracteres.")
+	@NotBlank(message = "Informe o nome do profissional.")
 	private String nome;
+	@NotBlank(message = "Informe o bairro do profissional.")
 	private String bairro;
+	@NotBlank(message = "Informe o data de nascimento do profissional.")
 	private String dataNascimento;
+	@NotBlank(message = "Informe o celular do profissional.")
 	private String celular;
+	@NotBlank(message = "Informe o telefone do profissional.")
 	private String telefone;
+	@Email(message = "Informe um endereço de email válido.")
+	@NotBlank(message = "Informe o email do profissional.")
 	private String email;
+	@CPF(message = "Informe um endereço de cpf válido.")
+	@NotBlank(message = "Informe o cpf do profissional.")
 	private String cpf;
+	@NotBlank(message = "Informe o sexo do profissional.")
 	private String sexo;
 	
 }
